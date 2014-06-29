fdaf1b6a12236cc0789a90cfe0a88a5f3208e2b0
Stefan Birkner <mail@stefan-birkner.de>
2014-06-29 14:29:38 +0200

    Render slides with JMarkdown2DeckJS.

--- src/main/java/com/github/stefanbirkner/slides2go/gist/ShowGist.java
+++ src/main/java/com/github/stefanbirkner/slides2go/gist/ShowGist.java
@@ -1,6 +1,7 @@
 package com.github.stefanbirkner.slides2go.gist;
 
-import com.github.stefanbirkner.slides2go.transformer.MarkdownTransformer;
+import com.github.stefanbirkner.jmarkdown2deckjs.Configuration;
+import com.github.stefanbirkner.jmarkdown2deckjs.JMarkdown2DeckJs;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -10,12 +11,12 @@ import spark.Route;
  */
 public class ShowGist implements Route {
     private static final GistSourceProvider GIST_SOURCE_PROVIDER = new GistSourceProvider();
-    private static final MarkdownTransformer TRANSFORMER = new MarkdownTransformer();
+    private static final JMarkdown2DeckJs TRANSFORMER = new JMarkdown2DeckJs(new Configuration("http://imakewebthings.com/deck.js/"));
 
     @Override
     public Object handle(Request request, Response response) {
         GistRequest gistRequest = new GistRequest(request.params(":user"), request.params(":id"));
         String markdown = GIST_SOURCE_PROVIDER.getSourceForRequest(gistRequest);
-        return TRANSFORMER.transform(markdown);
+        return TRANSFORMER.convert(markdown);
     }
 }
